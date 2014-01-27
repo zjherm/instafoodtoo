3d2a44512f8d6f35a0770294ac02b2cd7d049d9a
Zack Herman <zackjherman@gmail.com>
2014-01-27 11:50:50 -0800

    asdf

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
   , connect = require('connect')
-  , $ = require('jQuery')
 
 app.listen(3000);
 
