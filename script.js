406f8640dd7d05bc2cd57935eb4cd18df3192d48
zjherm <zackjherman@gmail.com>
2014-01-31 14:00:52 -0800

    asdf

--- script.js
+++ script.js
@@ -7,24 +7,13 @@
     , port = process.env.PORT || 5000;
 
   app.listen(port);
-
-  io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.emit('stuff', function() {
-      console.log("http server listening on %d", port);
-    });
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
-
-  });
+  app.set('view engine', 'ejs');
 
   // Defining Routes
 
-  app.configure(function(){
-    app.use('/media', express.static(__dirname + '/media'));
-    app.use(express.static(__dirname + '/public'));
-  });
+app.get('/', function(req, res) {
+  res.render('index', { title: 'The index page!' })
+});
 
   // app.get('/', function (req, res) {
   //   res.send('Testing');
