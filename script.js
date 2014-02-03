8e6ac90a453187bb27fb8ed85fe356a37b97de1e
Greg Thompson <gthompson@peoplelinx.com>
2014-02-03 12:26:18 -0500

    Fixes

--- script.js
+++ script.js
@@ -16,26 +16,15 @@ io.configure(function () {
   io.set("polling duration", 10); 
 });
 
-// SETTING UP EJS
+Instagram.set('client_id', '70393263f72f44cc9a3ef9786a4d389f');
+Instagram.set('client_secret', 'fa2725e2a08a4158bad297f35b5c6bec');
+Instagram.set('callback_url', 'http://instafood.herokuapp.com/endpoint');
 
-// Register ejs as .html. If we did
-// not call this, we would need to
-// name our views foo.ejs instead
-// of foo.html. The __express method
-// is simply a function that engines
-// use to hook into the Express view
-// system by default, so if we want
-// to change "foo.ejs" to "foo.html"
-// we simply pass _any_ function, in this
-// case `ejs.__express`.
+
 app.engine('.html', require('ejs').__express);
-
 // Optional since express defaults to CWD/views
 app.set('views', __dirname + '/views');
 
-// Without this you would need to
-// supply the extension to res.render()
-// ex: res.render('users.html').
 app.set('view engine', 'html');
 
 app.configure(function(){
@@ -46,24 +35,14 @@ app.configure(function(){
     app.use(express.errorHandler());
 });
 
-  // app.get('/endpoint', function (req, res) { 
-  //   // For convention's sake, we only respond to this if it's a properly formatted request from Instagram
-  //   if (req.query['hub.challenge']) {
-  //     // First we check if hub.challenge exists in the query, then we do something
-  //     res.send(req.query['hub.challenge']);
-  //   }
-  // });
-
-Instagram.set('client_id', '70393263f72f44cc9a3ef9786a4d389f');
-Instagram.set('client_secret', 'fa2725e2a08a4158bad297f35b5c6bec');
-Instagram.set('callback_url', 'http://instafood.herokuapp.com/endpoint');
-
 app.post('/endpoint', function (req, res) {
      var body = "";
         req.on('data', function (chunk) {
+          console.log('chunk');
           body += chunk;
         });
         req.on('end', function () {
+          console.log('end');
           getPhoto(body);
           res.writeHead(200);
           res.end();
