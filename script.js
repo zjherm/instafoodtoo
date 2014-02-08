ac35a479355036fe3b14f147fba6d6383aba745e
zjherm <zackjherman@gmail.com>
2014-02-07 21:41:20 -0800

    gmap

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ Instagram.set('callback_url', 'http://instafood.herokuapp.com/endpoint');
 
 
 // app.use(express.static(__dirname + '/'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/Public'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
