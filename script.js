e8feed7d3accbecab4800314c23321c4abead143
Zack Herman <zackjherman@gmail.com>
2014-03-05 09:23:54 -0800

    trying input field

--- script.js
+++ script.js
@@ -53,17 +53,22 @@ app.get('/', function(req, res){
     }
   });
   // Get Form Data
-  io.on('data', function(data) {
-    console.log("heres the hash " + data.hash);
+  io.sockets.on('connection', function(socket) {
+    io.on('data', function(data) {
+      console.log("heres the hash " + data.hash);
+    });
   });
+
 });
 
 
   // Get Form Data
-  io.on('data', function(data) {
-    console.log("heres the hash " + data.hash);
+  io.sockets.on('connection', function(socket) {
+    io.on('data', function(data) {
+      console.log("heres the hash " + data.hash);
+    });
   });
-
+  
 app.get('/set_sub', function(req, res) {
   // #food subscription
     Instagram.subscriptions.subscribe({ object: 'tag', object_id: 'foodporn' });
