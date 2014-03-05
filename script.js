4a33929478838c44a54c7da9b6c62ba54d5a87f0
Zack Herman <zackjherman@gmail.com>
2014-03-05 14:07:06 -0800

    getting disconnect working

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ var util = require('util'),
 var port = process.env.PORT || 3000;
 var server = app.listen(port);
 var io = require('socket.io').listen(server);
-var socket = io.connect('http://yourdomain.com', {'sync disconnect on unload' : true});
 
 //This is required to work on Heroku; it defaults to long polling; actual web-sockets not supported
 io.configure(function () { 
